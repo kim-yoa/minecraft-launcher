96d1769e04882e45c5416936b2248460eb8c7788
이호형 <75205559+leehohyung0117@users.noreply.github.com>
2024-09-16 00:17:40 +0900

    update 1.0.1

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://kimyoa.wstr.fr/nebula/uwa/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://server.kimyoa.com/minecraft/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
